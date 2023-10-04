2503b4229dc306401d773cbb9d684d987b82d568
RobertoRdg <rodriguezr61@uabc.edu.mx>
2023-10-03 19:11:21 -0700

    primera actividad con JS, calculadora basica con dos numeros

---
+++ practica24/practica24.js
@@ -0,0 +1,12 @@
+var num1 = parseInt(prompt("INGRESE EL PRIMER NUMERO"));
+var num2 = parseInt(prompt("INGRESE EL SEGUNDO NUMERO"));
+var suma = num1+num2;
+var resta = num1-num2;
+var multiplicacion = num1*num2;
+var division = num1/num2;
+var textoS = "EL RESULTADO DE LA SUMA ES: "
+var textoR = "EL RESULTADO DE LA RESTA ES: "
+var textoM = "EL RESULTADO DE LA MULTIPLICACION ES: "
+var textoD = "EL RESULTADO DE LA DIVISION ES: "
+
+alert( textoS+suma+", "+textoR+resta+", "+textoM+multiplicacion+", "+textoD+division);
