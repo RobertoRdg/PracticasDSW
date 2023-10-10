297fa20987f01327e9acaca25d712bfb29284d95
RobertoRdg <rodriguezr61@uabc.edu.mx>
2023-10-09 19:36:41 -0700

    realizamos sentencias if anidados en consola

---
+++ practica26/practica26.js
@@ -0,0 +1,10 @@
+var num1=5;
+var num2=8;
+
+if (num1 <= num2) {
+    console.log(num1+" NO ES MAYOR QUE "+num2);
+    } if (num2 >= 0) {
+        console.log(num2+" ES NUMERO POSITIVO");
+        } if(++num1<num2){
+        console.log("INCREMENTAR UNO EN "+num1+", NO LO HACE MAYOR QUE "+num2);
+}
